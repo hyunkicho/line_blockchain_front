f099fb3a1958da8bb893164d192c5a6534c85e29
tkdrn0842@naver.com <tkdrn0842@naver.com>
2020-11-24 19:42:24 +0900

    api/* proxy

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         // 프록시 설정
         proxy: {
             // 프록시 요청을 보낼 api의 시작 부분
-            '/api': {
+            '/api/*': {
                 // 프록시 요청을 보낼 서버의 주소
                 target: 'http://localhost:3000'
             }
